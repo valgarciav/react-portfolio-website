fa6212013ae3b7b873ea449a18d1072bcf941c46
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-09-11 22:29:03 -0400

    react icons
    icons in process of being added

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://linkedin.com" target="_blank">
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com" target="_blank"></a>
+      <a href="https://instagram.com" target="_blank"></a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
