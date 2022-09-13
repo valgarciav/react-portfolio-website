2ce6eac4af005eacaf31721dfcf8997012e301d4
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-09-13 19:19:28 -0400

    about me page
    image transition effect added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,58 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { AiOutlineFolderOpen } from "react-icons/ai";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ years Working </small>
+            </article>
+
+            <article className="client__card">
+              <FiUsers className="clients__icon" />
+              <h5>Clients</h5>
+              <small>0 Worldwide</small>
+            </article>
+
+            <article className="project__card">
+              <AiOutlineFolderOpen className="projects__icon" />
+              <h5>Projects</h5>
+              <small>3+ Completed projects</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore esse
+            numquam eligendi nulla distinctio, reiciendis ex dolorem. Ab quos
+            eius aliquid aut. Dolore pariatur quo ratione consequuntur
+            reiciendis voluptates facilis.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            {" "}
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
