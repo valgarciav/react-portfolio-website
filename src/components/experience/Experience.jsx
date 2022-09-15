2ec19c82b7c61b025a66ca19277798f26eaa831d
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-09-15 15:01:19 -0400

    services
    containers added

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,52 +4,51 @@ import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">
+    <section id='experience'>
       <h5> What Skills I Have</h5>
-      <h2>MY Experience</h2>
-
-
+      <h2>My Experience</h2>
+      
       <div className='container experience__container'>
       <div className="experience__fontend">
         <h3>Frontend Development</h3>
         <div className="experience__content">
           <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon' />
             <div>
             <h4>HTML</h4>
             <small className="text-light">Intermediate</small>
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>CSS</h4>
             <small className='text-light'>Intermediate</small>
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>JavaScript</h4>
             <small className='text-light'>Beginner</small>
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>Bootstrap</h4>
             <small className='text-light'>Beginner</small>
             </div>
             </article> 
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>React</h4>
             <small className='text-light'>Intermediate</small> 
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>Tailwind</h4>
             <small className='text-light'>Beginner</small> 
@@ -64,7 +63,7 @@ const Experience = () => {
       <h3>Backend Development</h3>
       <div className="experience__content">
           <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>Node JS
             </h4>
@@ -72,35 +71,35 @@ const Experience = () => {
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>MYSQL</h4>
             <small className='text-light'>Intermediate</small>
             </div>
             </article>
             <article className='experience__details'>
-              <div>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
+            <div>
             <h4>Express</h4>
             <small className='text-light'>Beginner</small>
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>Mongo DB</h4>
             <small className='text-light'>Beginner</small>
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>Python</h4>
             <small className='text-light'>Intermediate</small> 
             </div>
             </article>
             <article className='experience__details'>
-            <BsFillPatchCheckFill />
+            <BsFillPatchCheckFill className='experience__details-icon'/>
             <div>
             <h4>RESTFUL APIs</h4>
             <small className='text-light'>Beginner</small> 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,123 @@
 import React from "react";
 import "./services.css";
+import { BsCheckLg } from "react-icons/bs";
 
 const Services = () => {
-  return <section id="services"> Services </section>;
+  return (
+    <section id="services">
+      <h5> What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="services">
+          <div className="service__head">
+            <h3> UI/UX Design </h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* END OF UX/UI*/}
+
+        <article className="services">
+          <div className="service__head">
+            <h3>Web Development </h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+        {/*END OF WEB DEVELOPMENT*/}
+
+        <article className="services">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+
+            <li>
+              <BsCheckLg className="service__list-icon" />
+              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
