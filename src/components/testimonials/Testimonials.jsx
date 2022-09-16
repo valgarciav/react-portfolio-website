315e573b2d5d4b7774d9a322bc72f77f7130da16
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-09-16 07:38:53 -0400

    testimonials
    css added. pagination needs to show on outside still.

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -9,8 +9,8 @@ import { Pagination } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
+import "swiper/css";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 const data = [
   {
@@ -38,7 +38,15 @@ const Testimonials = () => {
       <h5>Review form clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper
+        className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
         {data.map(({ avatar, name, review }, index) => {
           return (
             <SwiperSlide key={index} className="testimonial">
@@ -51,7 +59,7 @@ const Testimonials = () => {
             </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
