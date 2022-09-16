826b18d52880473e7268a9c4201e42c9fc7fb5ad
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-09-16 06:50:18 -0400

    testimonials
    css added. array added, needs actual testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
 
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Jane Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad temporibus voluptate odit voluptates quas autem.",
+  },
+  {
+    avatar: AVTR2,
+    name: "John Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad temporibus voluptate odit voluptates quas autem.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Bob Doe",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad temporibus voluptate odit voluptates quas autem.",
+  },
+];
 const Testimonials = () => {
-  return <section id="testimonials"> Testimonials </section>;
+  return (
+    <section id="testimonials">
+      <h5>Review form clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <article key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} />
+              </div>
+
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
