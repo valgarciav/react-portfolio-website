a4ef60815d05462c2d04b00bb562cb2d889ba93b
valgarciav <97136074+valgarciav@users.noreply.github.com>
2022-09-17 10:27:20 -0400

    footer
    footer css added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
 import "./footer.css";
+import { BsInstagram } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Valeria Garcia
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://instagram.com">
+          <BsInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <BsTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Valeria Garcia. All rights reserved</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
